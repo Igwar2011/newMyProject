416599c7f25b5588cd1a57ca55f81d20e2dd2f01
Igwar <igwar2011@gmail.com>
2023-04-17 13:48:00 +0300

    added methods sum() and div()

---
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,14 @@
+public class App {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 7:
+        System.out.printin(sum(a, b));
+        System.out.printin(div(a, b));
+        public static int sum ( int a, int b){
+            return a + b;
+        }
+        public static int div ( int a, int b){
+            return a - b;
+        }
+    }
+}
